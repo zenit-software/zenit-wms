d2cfa4b12ed981b323d317067f1452b3c64e12b9
anto <a.nikic@gmx.at>
2025-11-28 21:50:14 +0100

    feat: initial spring boot project - first strike

---
+++ wms/src/main/java/dev/zenit/wms/ZenitWmsApplication.java
@@ -0,0 +1,13 @@
+package dev.zenit.wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenitWmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenitWmsApplication.class, args);
+	}
+
+}
---
+++ wms/src/test/java/dev/zenit/wms/ZenitWmsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zenit.wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenitWmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
